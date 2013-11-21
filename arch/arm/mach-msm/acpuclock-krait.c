b1b53ce1fc13ff2fe3b6cb08d5a3b96ee8605431
anarkia1976 <stefano.villa1976@gmail.com>
2013-11-21 14:35:16 +0100

    msm: krait: extend voltage control range

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -929,8 +929,8 @@ static void __init bus_init(const struct l2_level *l2_level)
 static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
 
 #ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
-#define CPU_VDD_MAX		1300
-#define CPU_VDD_MIN		700
+#define CPU_VDD_MAX		1450
+#define CPU_VDD_MIN		600
 
 static unsigned int cnt;
 
