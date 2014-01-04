938f9184be8d655265096e93c9c7ee1cde750e20
anarkia1976 <stefano.villa1976@gmail.com>
2014-01-05 00:15:34 +0100

    gpu: overclock: set 487Mhz by default

--- arch/arm/mach-msm/lge/mako/board-mako-gpu.c
+++ arch/arm/mach-msm/lge/mako/board-mako-gpu.c
@@ -228,7 +228,7 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 400000000,
+			.gpu_freq = 487500000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
