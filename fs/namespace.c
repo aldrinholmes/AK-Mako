da2c01055acd004da19c9fdfae2e500cab9a2728
dorimanx <yuri@bynet.co.il>
2014-01-15 15:24:30 +0200

    Always set noatime no diratime on all partitions! I/O Boost! V2

--- fs/namespace.c
+++ fs/namespace.c
@@ -2197,6 +2197,10 @@ long do_mount(char *dev_name, char *dir_name, char *type_page,
 	if (!(flags & MS_RELATIME))
 		mnt_flags |= MNT_NOATIME;
 
+	/* Default to noatime/nodiratime unless overriden */
+	if (!(flags & MS_RELATIME))
+		mnt_flags |= MNT_NOATIME;
+
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
 		mnt_flags |= MNT_NOSUID;
