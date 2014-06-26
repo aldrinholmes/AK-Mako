56164009cfb2d570938589375deab43216fcd17a
Stratos Karafotis <stratosk@semaphore.gr>
2014-06-26 22:18:15 +0300

    cpufreq: ondemand: Add down_threshold 5 to utilize min freq
    Add this heuristic to utilize min freq 384MHz instead of 405MHz on
    very low loads.
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -38,6 +38,7 @@
 #define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
 #define MIN_FREQUENCY_UP_THRESHOLD		(11)
 #define MAX_FREQUENCY_UP_THRESHOLD		(100)
+#define DEF_DOWN_THRESHOLD			(5)
 #define TOUCH_LOAD				(75)
 #define TOUCH_LOAD_THRESHOLD			(10)
 #define TOUCH_LOAD_DURATION			(1100)
@@ -781,10 +782,16 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 			this_dbs_info->rate_mult =
 				dbs_tuners_ins.sampling_down_factor;
 		dbs_freq_increase(policy, policy->max);
+	} else if (max_load < DEF_DOWN_THRESHOLD) {
+		/* No longer fully busy, reset rate_mult */
+		this_dbs_info->rate_mult = 1;
+
+		__cpufreq_driver_target(policy, policy->min,
+					CPUFREQ_RELATION_L);
 	} else {
 		/* Calculate the next frequency proportional to load */
 		unsigned int freq_next, min_f, max_f;
-		
+
 		min_f = policy->cpuinfo.min_freq;
 		max_f = policy->cpuinfo.max_freq;
 		freq_next = min_f + max_load * (max_f - min_f) / 100;
